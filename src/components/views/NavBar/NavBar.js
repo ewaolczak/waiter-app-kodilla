22370335f1fafa701c9e5444a274a65aff4c669d
Ewa Olczak <ewa.olczak@gmail.com>
2023-02-10 23:08:45 +0100

    Add objects to tables, add NavBar

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <Navbar
+        bg='primary'
+        variant='dark'
+        expand='lg'
+        className='mt-4 mb-4 rounded'>
+        <Container>
+          <Navbar.Brand className='ms-1'>Waiter.app</Navbar.Brand>
+          <Nav className='justify-content-end me-1'>
+            <Nav.Link as={NavLink} to='/'>
+              Home
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default NavBar;
