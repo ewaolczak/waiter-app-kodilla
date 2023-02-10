1ee3beb7a22fe5457f4ad501140f2075f04079a3
Ewa Olczak <ewa.olczak@gmail.com>
2023-02-10 23:47:06 +0100

    Add rendering list of tables

--- src/components/pages/TablesList/TableList.js
+++ src/components/pages/TablesList/TableList.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Card, Button, Row, Col } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getAllTables } from '../../../redux/tablesRedux';
 
 const TableList = () => {
-  return (
-    <div>
-      <h1>Tables List</h1>
-    </div>
-  )
-}
+  const tables = useSelector(getAllTables);
 
-export default TableList
+  return (
+    <>
+      <h1>All tables</h1>
+      {tables.map((table) => (
+        <Card>
+          <Card.Body>
+            <Row className='flex-column flex-md-row'>
+              <Col className='flex-row'>
+                <Col>
+                  <Card.Title className='fs-4'>Table {table.id}</Card.Title>
+                </Col>
+                <Col>
+                  <Card.Text as='p'>
+                    <strong>Status: </strong>
+                    {table.status}
+                  </Card.Text>
+                </Col>
+              </Col>
+              <Col>
+                <Link to={`/table/${table.id}`}>
+                  <Button variant='primary' className='text-nowrap'>
+                    Show more
+                  </Button>
+                </Link>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      ))}
+    </>
+  );
+};
+
+export default TableList;
